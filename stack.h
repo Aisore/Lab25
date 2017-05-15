a8c085da11abf7b872dcad638b0a2e87156f7f1b
Aisore <evgenia15678a@yandex.ru>
2017-05-15 17:12:10 +0300

    1-st

---
+++ stack.h
@@ -0,0 +1,20 @@
+
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef int Item;
+
+typedef struct _stack Stack;
+
+Stack *stack_create(size_t stack_size);
+void stack_push(Stack *s, Item value);
+Item stack_pop(Stack *s);
+bool stack_is_empty(Stack *s);
+void stack_destroy(Stack **s);
+
+Item stack_top(Stack *s);
+
+#endif // __STACK_H__
